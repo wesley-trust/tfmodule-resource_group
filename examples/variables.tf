dccda567ce68dce978a9a89be951d09274cd5cb6
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-09-08 18:29:21 +0100

    Update

--- examples/variables.tf
+++ examples/variables.tf
@@ -8,17 +8,17 @@ variable "service_name" {
 variable "service_environment" {
   description = "Desired environment for the service collection of provisioned resources"
   type        = string
-  default = "Test"
+  default     = "Test"
 }
 
 variable "service_deployment" {
   description = "Desired deployment identifier of the service collection of provisioned resources"
   type        = string
-  default = "01"
+  default     = "01"
 }
 
 variable "service_location" {
   description = "The production resource locations to deploy"
   type        = string
-  default = "UK South"
+  default     = "UK South"
 }
